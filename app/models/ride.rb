189345724a90395492cd3fc76a23e024e8b1e78c
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-02-15 23:07:32 -0800

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,22 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    short_tickets = (user.tickets < attraction.tickets)
+    short_height = (user.height < attraction.min_height)
+
+    if !short_tickets && !short_height
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
+      msg = "Thanks for riding the #{attraction.name}!"
+    else
+      msg = "Sorry."
+      msg += " You do not have enough tickets to ride the #{attraction.name}." if short_tickets
+      msg += " You are not tall enough to ride the #{attraction.name}." if short_height
+      msg
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-
-
+  root 'welcome#home'
+  get '/signin', to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
+  resources :users, only: [:new, :create, :show, :destroy]
+  resources :attractions
+  resources :rides, only: [:create]
 end
